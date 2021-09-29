ccbf5133699707e07f28bc8fe0600261d8ba3dba
weldi djapri <weldiyono.djapri@amarbank.co.id>
2021-09-29 15:51:56 +0700

    minor adjustment

--- src/components/movie-detail/movie-detail.jsx
+++ src/components/movie-detail/movie-detail.jsx
@@ -29,34 +29,32 @@ const MovieDetail = ({ id }) => {
       <main className="px-4">
         <div className="py-10 md:h-screen">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto"
             onClick={() => {
               history.push('/');
             }}>
             Back
           </button>
 
+          <img
+            src={movieData.Poster}
+            alt={movieData.Title}
+            className="h-100 w-80 mx-auto object-cover rounded-xl"
+          />
           <div className="flex align-center justify-center mt-5">
             <h1 className=" font-bold text-2xl my-1 align-center">
               {movieData.Title}
             </h1>
           </div>
-          <div className="flex flex-col items-center my-10 md:flex-row ">
-            <img
-              src={movieData.Poster}
-              alt={movieData.Title}
-              className="h-100 w-80 mx-auto object-cover rounded-xl"
-            />
-            <div className="flex flex-col md:ml-4 md:w-1/2">
+          <div className="flex justify-center items-center my-10 md:flex-row ">
+            <div className="flex flex-col justify-center align-center md:ml-4 md:w-1/2">
               <div className="flex flex-wrap my-1">
                 <h1 className="font-bold text-base">Actors:</h1>
-                <span className="font-normal md:mx-1">{movieData.Actors}</span>
+                <span className="mx-1">{movieData.Actors}</span>
               </div>
               <div className="flex flex-wrap my-1">
                 <h1 className="font-bold text-base">Directors:</h1>
-                <span className="font-normal md:mx-1">
-                  {movieData.Director}
-                </span>
+                <span className="mx-1">{movieData.Director}</span>
               </div>
               <div className="flex flex-wrap my-1">
                 {movieData.Ratings.length > 0 && (
@@ -68,39 +66,39 @@ const MovieDetail = ({ id }) => {
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Showing At:</h1>
-                <span className="font-normal mx-1">{movieData.Country}</span>
+                <span className="mx-1">{movieData.Country}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Type:</h1>
-                <span className="font-normal mx-1">{movieData.Type}</span>
+                <span className="mx-1">{movieData.Type}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Released:</h1>
-                <span className="font-normal mx-1">{movieData.Released}</span>
+                <span className="mx-1">{movieData.Released}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Duration:</h1>
-                <span className="font-normal mx-1">{movieData.Runtime}</span>
+                <span className="mx-1">{movieData.Runtime}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Genre:</h1>
-                <span className="font-normal mx-1">{movieData.Genre}</span>
+                <span className="mx-1">{movieData.Genre}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Awards:</h1>
-                <span className="font-normal mx-1">{movieData.Awards}</span>
+                <span className="mx-1">{movieData.Awards}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">Rated:</h1>
-                <span className="font-normal mx-1">{movieData.Rated}</span>
+                <span className="mx-1">{movieData.Rated}</span>
               </div>
               <div className="flex my-1">
                 <h1 className="font-bold text-base">IMDB Rating:</h1>
-                <span className="font-normal mx-1">{movieData.imdbRating}</span>
+                <span className="mx-1">{movieData.imdbRating}</span>
               </div>
               <div className="flex flex-wrap my-1">
                 <h1 className="font-bold text-base">Descriptions:</h1>
-                <span className="font-normal">{movieData.Plot}</span>
+                <span>{movieData.Plot}</span>
               </div>
             </div>
           </div>
--- src/shared/components/navbar/navbar.jsx
+++ src/shared/components/navbar/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   return (
     <div className="bg-blue-600 h-16" data-testid="Navbar">
-      <div className="flex flex-row justify-start align-start max-w-6xl mx-auto p-5">
+      <div className="max-w-6xl p-5">
         <h1
           className="text-white font-bold text-3xl cursor-pointer"
           onClick={() => {
